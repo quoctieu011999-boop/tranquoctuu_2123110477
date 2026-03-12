a02ff14a8c78ea1b7aaf654d7eeac4c87c0dc104
Admin <Admin@D20810>
2026-03-12 13:36:26 +0700

    Add ProductController

---
+++ tranquoctuu_2123110477/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tranquoctuu_2123110477.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
